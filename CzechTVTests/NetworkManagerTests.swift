37c0f6eb04ec9809cc7a5222f841ec9829c56fd2
Marcel Mravec <marcel.mravec@gmail.com>
2023-10-23 15:24:34 +0200

    fix improved unit tests

--- CzechTVTests/NetworkManagerTests.swift
+++ CzechTVTests/NetworkManagerTests.swift
@@ -10,10 +10,11 @@ import XMLCoder
 @testable import CzechTV
 
 class NetworkManagerTests: XCTestCase {
-
+    
     var sut: NetworkManager!
     var mockSession: MockURLSession!
-    
+    let mockProgramXMLFileName = "MockProgram.xml"
+
     override func setUp() {
         super.setUp()
         mockSession = MockURLSession()
@@ -26,42 +27,30 @@ class NetworkManagerTests: XCTestCase {
         super.tearDown()
     }
     
+    func getMockProgramData() -> Data? {
+        if let url = Bundle(for: type(of: self)).url(forResource: "MockProgram", withExtension: "xml"),
+           let data = try? Data(contentsOf: url) {
+            return data
+        }
+        XCTFail("Failed to get mock data from \(mockProgramXMLFileName)")
+        return nil
+    }
+    
     func testParseMockProgram() {
-        // 1. Get the URL for MockProgram.xml from the test bundle
-        guard let url = Bundle(for: type(of: self)).url(forResource: "MockProgram", withExtension: "xml") else {
-            XCTFail("Missing file: MockProgram.xml")
-            return
-        }
+        guard let data = getMockProgramData() else { return }
 
-        // 2. Load the XML data
-        guard let data = try? Data(contentsOf: url) else {
-            XCTFail("Unable to load XML data from MockProgram.xml")
-            return
-        }
-
-        // 3. Parse the XML data
-        let decoder = XMLDecoder()
         do {
-            let decodedData = try decoder.decode(Program.self, from: data)
-            // Now use decodedData to run your tests
+            let decodedData = try XMLDecoder().decode(Program.self, from: data)
+            XCTAssertEqual(decodedData.porad.first?.nazvy.nazev, "Studio 6")
         } catch {
             XCTFail("Decoding failed: \(error)")
         }
     }
 
-    
     func testFetchData_Success() {
-        guard let url = Bundle(for: type(of: self)).url(forResource: "MockProgram", withExtension: "xml") else {
-            XCTFail("Missing file: MockProgram.xml")
-            return
-        }
-        guard let data = try? Data(contentsOf: url) else {
-            XCTFail("Unable to load XML data from MockProgram.xml")
-            return
-        }
-        let mockData = data // ... provide some mock XML data here
+        guard let mockData = getMockProgramData() else { return }
         mockSession.data = mockData
-        
+
         sut.fetchData(date: Date(), channel: .ct1)
         
         XCTAssertNotNil(sut.program)
@@ -69,32 +58,22 @@ class NetworkManagerTests: XCTestCase {
     }
     
     func testFetchData_DecodingError() {
-        // Create an expectation for a background task.
         let expectation = XCTestExpectation(description: "Decode data")
-
-        // Set up mock data and session
-        let mockData = Data("Invalid XML".utf8)
-        mockSession.data = mockData
-
-        // Inject the mock session into the SUT (System Under Test)
-        sut = NetworkManager(session: mockSession)
-
-        // Fetch data and wait for the asynchronous process to complete
+        
+        mockSession.data = Data("Invalid XML".utf8)
+        
         sut.fetchData(date: Date(), channel: .ct1) {
-            // Asserts
             XCTAssertNotNil(self.sut.error)
             if case .decodingError = self.sut.error {
-                print(self.sut.error?.localizedDescription)
+                print(self.sut.error?.localizedDescription ?? "Unknown error")
             } else {
                 XCTFail("Expected decoding error")
             }
             expectation.fulfill()
         }
-
-        // Wait for the expectation to be fulfilled
+        
         wait(for: [expectation], timeout: 10.0)
     }
 
-    
-    // Add more tests for other error scenarios...
 }
+
